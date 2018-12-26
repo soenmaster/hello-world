94942f804c11f82598e7d9a7d473c809d6cc68b1
soenmaster <46168678+soenmaster@users.noreply.github.com>
2018-12-27 06:57:07 +0900

    파일 추가
    아무 파일이나 추가해 봄

---
+++ PtTestSelf.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#define _TIMESPEC_DEFINED
+#include "pthread.h"
+
+#pragma comment(lib, "pthreadVC2.lib")
+
+void *thread(void *thread_id) {
+
+	for (int i = 0; i < 20; i++) {
+		printf(".");
+		Sleep(200);
+	}
+
+	return NULL;
+}
+
+int main() {
+	pthread_t worker;
+	pthread_create(&worker, NULL, thread, (char *)"Worker");
+	for (int i = 0; i < 20; i++) {
+		printf("M");
+		Sleep(300);
+	}
+}
